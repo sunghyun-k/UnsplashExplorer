37faba10d34c22e41f2754e53319b187e1f31a28
Sunghyun Kim <sunghyun_kim@jazzhallabong.com>
2022-03-22 04:03:01 +0900

    UnplashAPI 파일 생성

---
+++ UnsplashExplorer/UnsplashAPI.swift
@@ -0,0 +1,14 @@
+//
+//  UnsplashAPI.swift
+//  UnsplashExplorer
+//
+//  Created by Sunghyun Kim on 2022/03/22.
+//
+
+import Foundation
+
+struct UnsplashAPI {
+    static let scheme = "https"
+    static let host = "api.unsplash.com"
+    
+}
